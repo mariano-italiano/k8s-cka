f04add999f525fb62c93df22b8c88e5c5b2e5a15
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-04-23 09:19:11 +0200

    Update deployK8s.sh

--- deployK8s.sh
+++ deployK8s.sh
@@ -11,9 +11,9 @@ then
                 esac
         done
 else
-        echo "Usage:  ./deployK8s.sh -v <k8s-version> -p <proxyHost:proxyPort>"
-	echo	
- 	echo "Examples: ./deployK8s.sh -v 1.28.9-2.1 -p http://172.22.60.38:3128"
+        echo "Usage:  ./deployK8s.sh -v <k8s-version> -p <proxyHost:proxyPort> <master-IP>"
+        echo
+        echo "Examples: ./deployK8s.sh -v 1.28.9-2.1 -p http://172.22.60.38:3128 172.22.48.200"
         exit 0
 fi
 
@@ -82,74 +82,87 @@ apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
 
 if [[ $(hostname) =~ .*master.* ]]
 then
-	if [[ $# -gt 3 ]]; then
+        if [[ $# -gt 3 ]]; then
                 echo "HTTP_PROXY=$4" >> /etc/environment
                 echo "HTTPS_PROXY=$4" >> /etc/environment
                 echo "http_proxy=$4" >> /etc/environment
                 echo "https_proxy=$4" >> /etc/environment
-                echo "NO_PROXY=10.96.0.0/12,192.168.0.0/16,172.22.48.212" >> /etc/environment
+                echo "NO_PROXY=10.96.0.0/12,192.168.0.0/16,$5" >> /etc/environment
                 sed -i '/\[Service\]/aEnvironment="HTTP_PROXY='"$4"'"' /lib/systemd/system/containerd.service
                 sed -i '/\[Service\]/aEnvironment="HTTPS_PROXY='"$4"'"' /lib/systemd/system/containerd.service
-                sed -i '/\[Service\]/aEnvironment="NO_PROXY="10.96.0.0/12,192.168.0.0/16,172.22.48.212"' /lib/systemd/system/containerd                                                                            .service
+                sed -i '/\[Service\]/aEnvironment="NO_PROXY="10.96.0.0/12,192.168.0.0/16,'"$5"'"' /lib/systemd/system/containerd.service
                 systemctl daemon-reload
                 systemctl restart containerd.service
-	fi
- 
+        fi
+
         echo "[TASK 12] Initialize the cluster"
-	#if [[ $# -gt 3 ]]; then
-        #        systemctl set-environment HTTP_PROXY=$4
-        #        systemctl set-environment HTTPS_PROXY=$4
-        #        systemctl restart containerd.service
-        #fi
-
         kubeadm init --pod-network-cidr 192.168.0.0/16 >/dev/null 2>&1
-
         sleep 10
 
         echo "[TASK 13] Set kubectl access"
         mkdir -p /home/student/.kube >/dev/null 2>&1
-        cp -i /etc/kubernetes/admin.conf /home/student/.kube/config >/dev/null 2>&1 
+        cp -i /etc/kubernetes/admin.conf /home/student/.kube/config >/dev/null 2>&1
         chown -R student:student /home/student/.kube >/dev/null 2>&1
-	#mkdir -p $HOME/.kube
-	#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-	#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+        #mkdir -p $HOME/.kube
+        #sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+        #sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
- 	#echo "[TASK 14] Install Antrea Network Plugin"
-  	#if [[ $# -gt 3 ]]; then
-   	#	curl -x $4 -fsSL https://github.com/antrea-io/antrea/releases/download/v1.15.1/antrea.yml | sudo tee antrea.yml
-     	#else
-      	#	curl -fsSL https://github.com/antrea-io/antrea/releases/download/v1.15.1/antrea.yml | sudo tee antrea.yml
-      	#fi
-	
-  	#kubectl apply -f antrea.yml
+        #################### CILIUM ###################
+        if [[ $# -gt 3 ]]; then
+                CILIUM_CLI_VERSION=$(curl -x $4 -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+        else
+                CILIUM_CLI_VERSION=$(curl  -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+        fi
+        CLI_ARCH=amd64
+        if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+        if [[ $# -gt 3 ]]; then
+                curl -x $4 -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+        else
+                curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+        fi
+        sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+        sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+        rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+        
+        https_proxy=$4 cilium install --version 1.15.4
 
+        #################### ANTREA ###################
+        #echo "[TASK 14] Install Antrea Network Plugin"
+        #if [[ $# -gt 3 ]]; then
+        #       curl -x $4 -fsSL https://github.com/antrea-io/antrea/releases/download/v1.15.1/antrea.yml | sudo tee antrea.yml
+        #else
+        #       curl -fsSL https://github.com/antrea-io/antrea/releases/download/v1.15.1/antrea.yml | sudo tee antrea.yml
+        #fi
+        #kubectl apply -f antrea.yml
+
+        #################### CALICO ###################
         #echo "[TASK 14] Install Calico networking"
-	#su -c 'kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml' student >/dev/null 2>&1
-	#sed -i 's/# - name: CALICO_IPV4POOL_CIDR/- name: CALICO_IPV4POOL_CIDR/g' calico.yaml >/dev/null 2>&1
-	#sed -i 's|#   value: "192.168.0.0/16"|  value: "192.168.0.0/16"|g' calico.yaml >/dev/null 2>&1
+        #su -c 'kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml' student >/dev/null 2>&1
+        #sed -i 's/# - name: CALICO_IPV4POOL_CIDR/- name: CALICO_IPV4POOL_CIDR/g' calico.yaml >/dev/null 2>&1
+        #sed -i 's|#   value: "192.168.0.0/16"|  value: "192.168.0.0/16"|g' calico.yaml >/dev/null 2>&1
         #kubectl apply -f calico.yaml >/dev/null 2>&1
 
 fi
 
 if [[ $(hostname) =~ .*worker.* ]]
 then
-	if [[ $# -gt 3 ]]; then
+        if [[ $# -gt 3 ]]; then
                 echo "HTTP_PROXY=$4" >> /etc/environment
                 echo "HTTPS_PROXY=$4" >> /etc/environment
                 echo "http_proxy=$4" >> /etc/environment
                 echo "https_proxy=$4" >> /etc/environment
-                echo "NO_PROXY=10.96.0.0/12,192.168.0.0/16,172.22.48.212" >> /etc/environment
+                echo "NO_PROXY=10.96.0.0/12,192.168.0.0/16,$5" >> /etc/environment
                 sed -i '/\[Service\]/aEnvironment="HTTP_PROXY='"$4"'"' /lib/systemd/system/containerd.service
                 sed -i '/\[Service\]/aEnvironment="HTTPS_PROXY='"$4"'"' /lib/systemd/system/containerd.service
-                sed -i '/\[Service\]/aEnvironment="NO_PROXY="10.96.0.0/12,192.168.0.0/16,172.22.48.212"' /lib/systemd/system/containerd.service
+                sed -i '/\[Service\]/aEnvironment="NO_PROXY="10.96.0.0/12,192.168.0.0/16,'"$5"'"' /lib/systemd/system/containerd.service
                 systemctl daemon-reload
                 systemctl restart containerd.service
-	fi
+        fi
         echo "[TASK 12] Join node to Kubernetes cluster"
         echo ""
         echo "Please execute following command to get join command and then execute it manually on all worker nodes"
         echo "-----------------------------------------------"
-	echo "kubeadm token create --print-join-command"
-	echo "-----------------------------------------------"
-	echo ""
+        echo "kubeadm token create --print-join-command"
+        echo "-----------------------------------------------"
+        echo ""
 fi
